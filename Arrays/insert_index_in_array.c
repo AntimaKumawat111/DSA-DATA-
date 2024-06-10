ccd0f3c513126cc488b5474a861dd36025f1e0e6
AntimaKumawat <aktank810>
2024-06-10 10:47:44 +0530

    insert_index_in_array

---
+++ Arrays/insert_index_in_array.c
@@ -0,0 +1,43 @@
+// ek nyi index ko purane array me inter karwana
+
+#include <stdio.h>
+void triversalArray();
+int insertIndex(int array[], int index, int element, int capacity, int size);
+
+int main()
+{
+    int array[100] = {2, 4, 5, 5, 3};
+    int index, element;
+    printf("Index number : ");
+    scanf("%d", &index);
+    printf("Element number : ");
+    scanf("%d", &element);
+    int size = 5;
+    triversalArray(array, size);
+    insertIndex(array, index, element, 100, size);
+    size++;
+    triversalArray(array, size);
+
+    return 0;
+}
+void triversalArray(int array[], int size)
+{ // for trivarse an array
+    for (int i = 0; i < size; i++)
+        printf("%d\t", array[i]);
+    printf("\n");
+}
+
+int insertIndex(int array[], int index, int element, int capacity, int size)
+{
+    if (size >= capacity)
+    {
+        return -1;
+    }
+
+    for (int i = size - 1; i >= index; i--)
+    {
+        array[i + 1] = array[i];
+    }
+    array[index] = element;
+    return 1;
+}
